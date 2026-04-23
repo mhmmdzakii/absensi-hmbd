133ce19fba4e9f290b77f3319b04a6cbfa85d810
mhmmdzakii <mhmmdzzkii@gmail.com>
2026-04-23 22:18:27 +0700

    Menambahkan fitur PWA untuk HP

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development", // PWA mati saat ngoding biar enteng
+  register: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Kalau sebelumnya ada pengaturan bawaan Next.js, taruh di dalam sini.
+  // Kalau kosong, biarkan saja seperti ini.
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
